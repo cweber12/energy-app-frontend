f82a92ded99fa7eb7e6a9195ce4f50fe74907764
coled <cweber33@uw.edu>
2026-01-26 22:51:11 -0800

    fix: wrap app in HashRouter to support Pages subpath

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,14 @@
 // filepath: src/index.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
